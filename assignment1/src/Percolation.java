966cea5d946c7353c30bb747e63f4371b24823b2
zurichxsl <zurichxsl@gmail.com>
2015-07-20 22:20:23 -0700

    assignment 1 wip

---
+++ assignment1/src/Percolation.java
@@ -0,0 +1,18 @@
+/**
+ * Created by sxu on 7/20/15.
+ */
+public class Percolation {
+
+//    public Percolation(int N)               // create N-by-N grid, with all sites blocked
+//    public void open(int i, int j)          // open site (row i, column j) if it is not open already
+//    public boolean isOpen(int i, int j)     // is site (row i, column j) open?
+//    public boolean isFull(int i, int j)     // is site (row i, column j) full?
+//    public boolean percolates()             // does the system percolate?
+
+    // test client (optional)
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+
+}
---
+++ assignment1/src/PercolationStats.java
@@ -0,0 +1,16 @@
+/**
+ * Created by sxu on 7/20/15.
+ */
+public class PercolationStats {
+//    public PercolationStats(int N, int T)     // perform T independent experiments on an N-by-N grid
+//    public double mean()                      // sample mean of percolation threshold
+//    public double stddev()                    // sample standard deviation of percolation threshold
+//    public double confidenceLo()              // low  endpoint of 95% confidence interval
+//    public double confidenceHi()              // high endpoint of 95% confidence interval
+
+    // test client (optional)
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
